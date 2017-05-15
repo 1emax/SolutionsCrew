d6fcaf36152187753f3b8512922e2981408bf51a
Artem <artem.bernatskyy@gmail.com>
2017-05-15 17:41:53 +0300

    added description && photos

--- cactusproj/templates/rawtemplate/js/detailed.js
+++
@@ -1,9 +0,0 @@
-function initMap() {
-    var centerLatLng = new google.maps.LatLng(59.2650709, 30.2522799);
-    var mapOptions = {
-        center: centerLatLng,
-        zoom: 11
-    };
-    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-}
-google.maps.event.addDomListener(window, "load", initMap);
